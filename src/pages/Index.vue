13f6c8b9aba20a417f42edbe3e4651b2747f2aa8
Valentine <dulzura64@gmail.com>
2021-06-29 16:07:04 -0500

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+
+    <q-btn round>
+      <q-avatar size="42px">
+        <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+      </q-avatar>
+    </q-btn>
+
+    <q-btn color="indigo" no-caps>
+      Multiline<br>Button
+    </q-btn>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
